bded6970cda8aab11fd798930a5a6b5ce9039021
Тимур Шафигуллин <timursafigullin@Timurs-Mac.local>
2017-10-06 12:45:54 +0300

    Added alerts, create news function

---
+++ VKProfile/Protocols/OnMoreClickListener.swift
@@ -0,0 +1,31 @@
+//
+//  OnMoreClickListener.swift
+//  VKProfile
+//
+//  Created by Тимур Шафигуллин on 06.10.17.
+//  Copyright © 2017 iOS Lab ITIS. All rights reserved.
+//
+
+import UIKit
+
+protocol OnMoreClickListener {
+    
+}
+
+extension OnMoreClickListener {
+    
+    func alert() -> UIAlertController {
+        let editProfile = "Редактировать профиль"
+        let copyLink = "Скопировать ссылку"
+        let share = "Поделиться..."
+        let cancel = "Отмена"
+        
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: editProfile, style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: copyLink, style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: share, style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: cancel, style: .cancel, handler: nil))
+        return alert
+    }
+    
+}
